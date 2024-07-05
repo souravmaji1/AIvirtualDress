de641cd93a221f86c377cf0a7c3ca5cc8d59ed12
sourav maji <majisourav255@gmail.com>
2024-07-05 18:08:31 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
-
+import Head from "next/head";
 
 
 
@@ -23,10 +23,22 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <head>
+        <Head>
         <meta name="google-site-verification" content="vMeZCAopXXa2mFglipcO1IqdC4QAN8lZzybOaUbibl8" />
         <script src="https://cdn.jsdelivr.net/npm/@gradio/client/dist/index.min.js"></script>
-        </head>
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-SWE283609G"
+            strategy="afterInteractive"
+          />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SWE283609G');
+            `}
+        </Script>
+        </Head>
         <body>
           <Toaster />
           {children}
